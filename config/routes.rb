2637d7818f523490cc7d355c4255d68fee3b895b
egifufox <acrobat.1205@gmail.com>
2026-01-04 15:00:16 +0900

    車両詳細・一覧表示機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#top'
 
-  resources :vehicles, only: [:new, :create, :show]
-  # 👇 この行を追加（後で車両一覧ページに変更予定）
-  # root "home#index"  # ← 今は仮で設定
+  resources :vehicles, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
