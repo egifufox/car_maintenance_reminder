bc10ce58c6b5b1eded80acd7839e769ed11f074d
egifufox <acrobat.1205@gmail.com>
2026-01-12 17:48:58 +0900

    feat: オイル交換記録の編集・削除機能を実装

--- app/controllers/oil_change_records_controller.rb
+++ app/controllers/oil_change_records_controller.rb
@@ -1,5 +1,6 @@
 class OilChangeRecordsController < ApplicationController
   before_action :set_vehicle
+  before_action :set_oil_change_record, only: [:edit, :update, :destroy]
 
   def index
     @oil_change_records = @vehicle.oil_change_records.order(changed_at: :desc)
@@ -13,17 +14,38 @@ class OilChangeRecordsController < ApplicationController
     @oil_change_record = @vehicle.oil_change_records.build(oil_change_record_params)
 
     if @oil_change_record.save
-      redirect_to vehicle_path(@vehicle), success: t('oil_change_records.create.success')
+      redirect_to vehicle_oil_change_records_path(@vehicle), success: t('oil_change_records.create.success')
     else
       flash.now[:danger] = t('oil_change_records.create.failure')
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @oil_change_record.update(oil_change_record_params)
+      redirect_to vehicle_oil_change_records_path(@vehicle), success: t('defaults.flash_message.updated', item: OilChangeRecord.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.flash_message.not_updated', item: OilChangeRecord.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @oil_change_record.destroy!
+    redirect_to vehicle_oil_change_records_path(@vehicle), success: t('defaults.flash_message.deleted', item: OilChangeRecord.model_name.human), status: :see_other
+  end
+
   private
 
   def set_vehicle
     @vehicle = current_user.vehicles.find(params[:vehicle_id])
+  end
+
+  def set_oil_change_record
+    @oil_change_record = @vehicle.oil_change_records.find(params[:id])
   end
 
   def oil_change_record_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'static_pages#top'
 
   resources :vehicles, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
-    resources :oil_change_records, only: [:index, :new, :create]
+    resources :oil_change_records, only: [:index, :new, :create, :edit, :update, :destroy]
   end
 end
