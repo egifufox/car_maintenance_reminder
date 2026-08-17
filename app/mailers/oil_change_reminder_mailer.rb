a264f05a8929c0d3dccf410299d48e598a137af4
egifufox <acrobat.1205@gmail.com>
2026-08-17 18:34:21 +0900

    Fix: メール内容

--- app/mailers/oil_change_reminder_mailer.rb
+++ app/mailers/oil_change_reminder_mailer.rb
@@ -5,8 +5,22 @@ class OilChangeReminderMailer < ApplicationMailer
   def reminder(user, vehicle)
     @user = user
     @vehicle = vehicle
-    @km_until_next_oil_change = vehicle.km_until_next_oil_change
-    @days_until_next_oil_change = vehicle.days_until_next_oil_change
+
+    # 走行距離ベースのフォーマット
+    km_until = vehicle.km_until_next_oil_change
+    @km_until_next_oil_change_text = if km_until < 0
+                                        "#{km_until.abs}km超過"
+                                      else
+                                        "あと#{km_until} km"
+                                      end
+
+    # 期間ベースのフォーマット
+    days_until = vehicle.days_until_next_oil_change
+    @days_until_next_oil_change_text = if days_until < 0
+                                          "#{days_until.abs}日超過"
+                                        else
+                                          "あと#{days_until}日"
+                                        end
 
     mail(
       to: @user.email,
